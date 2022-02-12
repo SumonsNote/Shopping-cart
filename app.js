ed472a179a25981070ffb31c3cdfb9b4160e2abb
Sumonsnote <sohankhance@gmail.com>
2022-02-13 01:52:53 +0600

    total

--- app.js
+++ app.js
@@ -9,8 +9,29 @@ function updateProductNumber(product, price, isIncreasing) {
   ProductInput.value = ProductNumber;
 
   const ProductTotal = document.getElementById(product + "-total");
-  ProductTotal.innerText = ProductNumber * price;
+    ProductTotal.innerText = ProductNumber * price;
+    calculateTotal()
 }
+
+function getInputValue(product) {
+    const phoneInput = document.getElementById(product + '-number');
+    const phoneNumber = parseInt(phoneInput.value);
+    return phoneNumber;
+}
+
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const taxAmount = subTotal / 10;
+    const totalPrice = subTotal + taxAmount;
+
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = taxAmount;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+// For phone
 document.getElementById('phone-plus').addEventListener('click', function () {
     updateProductNumber('phone', 1219, true);
 })
@@ -18,6 +39,7 @@ document.getElementById('phone-minus').addEventListener('click', function () {
     updateProductNumber('phone', 1219, false);
 })
 
+// for phone case
 document.getElementById("case-plus").addEventListener("click", function () {
   updateProductNumber('case', 59, true);
 });
